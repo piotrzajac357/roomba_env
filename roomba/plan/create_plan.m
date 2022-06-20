1abb240290965b9519286cf0d3fe4897122a8606
piotrzajac357 <38423119+piotrzajac357@users.noreply.github.com>
2022-06-20 18:35:36 +0200

    new_plan

--- roomba/plan/create_plan.m
+++ roomba/plan/create_plan.m
@@ -1,12 +1,12 @@
 clear all; close all;
 
-img = imread('plan6.png');
+img = imread('plan7.png');
 imshow(img);
 img = mat2gray(img);
 img = imresize(img,[200 200], 'nearest');
 
 M = img(:,:,1);
-fid = fopen('plan_binary1.txt', 'w+');
+fid = fopen('plan_binary2.txt', 'w+');
 for i=1:size(M, 1)
     fprintf(fid, '%d', M(i,:));
     fprintf(fid, '\n');
