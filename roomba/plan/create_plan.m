1ff4953898413bc579728ae0973a3c0b99a37271
piotrzajac357 <38423119+piotrzajac357@users.noreply.github.com>
2022-08-25 17:53:39 +0200

    plan_clover_added

--- roomba/plan/create_plan.m
+++ roomba/plan/create_plan.m
@@ -1,14 +1,16 @@
 clear all; close all;
 
-img = imread('plan7.png');
+img = imread('plan_clover.png');
 imshow(img);
 img = mat2gray(img);
-img = imresize(img,[2000 2000], 'nearest');
+img = imresize(img,[200 200], 'nearest');
 
 M = img(:,:,1);
-fid = fopen('plan_binary7_1cm.txt', 'w+');
+fid = fopen('plan_clover.txt', 'w+');
 for i=1:size(M, 1)
     fprintf(fid, '%d', M(i,:));
-    fprintf(fid, '\n');
+    if i~=size(M,1) 
+        fprintf(fid, '\n');
+    end
 end
 fclose(fid);
