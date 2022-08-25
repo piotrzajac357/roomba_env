e3b2d9cc505bf6b220e75bff2c054f106c03abaa
piotrzajac357 <38423119+piotrzajac357@users.noreply.github.com>
2022-08-25 18:11:03 +0200

    even_better_clover

--- roomba/plan/create_plan.m
+++ roomba/plan/create_plan.m
@@ -7,10 +7,14 @@ img = imresize(img,[200 200], 'nearest');
 
 M = img(:,:,1);
 fid = fopen('plan_clover.txt', 'w+');
-for i=1:size(M, 1)
+for i=2:size(M, 1)
     fprintf(fid, '%d', M(i,:));
     if i~=size(M,1) 
         fprintf(fid, '\n');
+    else
+        fprintf(fid,'\n');
+        fprintf(fid, '%d',M(1,:));
     end
+
 end
 fclose(fid);
