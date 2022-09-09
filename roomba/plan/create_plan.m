0c051b0d2734c669eb0be40abe4618bdb870fc37
piotrzajac357 <38423119+piotrzajac357@users.noreply.github.com>
2022-09-09 22:18:48 +0200

    qi_analysis_and_plans

--- roomba/plan/create_plan.m
+++ roomba/plan/create_plan.m
@@ -1,14 +1,14 @@
 clear all; close all;
 
-img = imread('plan_obstacles.png');
+img = imread('plan_narrow.png');
 imshow(img);
 img = mat2gray(img);
-imwrite(img, 'plan_obstacles.png');
+imwrite(img, 'plan_narrow.png');
 
 img = imresize(img,[200 200], 'nearest');
 
 M = img(:,:,1);
-fid = fopen('plan_obstacles.txt', 'w+');
+fid = fopen('plan_narrow.txt', 'w+');
 for i=2:size(M, 1)
     fprintf(fid, '%d', M(i,:));
     if i~=size(M,1) 
